9531b66a566fe1f220029a0850661ae6ff8753b1
20HDMI40 <heropista@gmail.com>
2022-07-23 12:26:26 +0200

    commit

---
+++ statistics.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
+document.querySelector("#close1").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
